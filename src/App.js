2a6ff99b41ad961c0d2b4a10d9b4b6502cacc8fe
fernando guerrero campos <fernandoguerreroc65@gmail.com>
2020-03-18 21:23:55 -0500

    created router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Layout } from 'antd';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+
+//@ Pages
+import { Home } from './pages/home';
+import { Movie } from './pages/movie';
+import { Popular } from './pages/popular';
+import { SearchMovie } from './pages/search-movie';
+import { NewMovies } from './pages/new-movie';
+import { Error404 } from './pages/error404';
+
 function App() {
+  const { Header, Content } = Layout;
   return (
-    <div className="App">
-      <h1>MOVIES</h1>
-    </div>
+    <Layout>
+      <Header>Header</Header>
+      <Content>
+        <Router basename="/">
+          <Switch>
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/new-movies" exact={true} component={NewMovies} />
+            <Route path="/popular" exact={true} component={Popular} />
+            <Route path="/search" exact={true} component={SearchMovie} />
+            <Route path="/movie/:id" exact={true} component={Movie} />
+            <Route path="*" exact={true} component={Error404} />
+          </Switch>
+        </Router>
+      </Content>
+    </Layout>
   );
 }
 
